becc0d89b118a1cdf9a821e787f2d44116a21497
EnriqueEsc <Spart117MX@hotmail.com>
2026-03-06 11:35:08 -0600

    Cambios para modelos personalizados

--- public/app.js
+++ public/app.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // 3. Le indicamos la ruta del archivo .glb
     // Asegúrate de cambiar 'tu-modelo.glb' por el nombre exacto de tu archivo
-    modeloPersonalizado.setAttribute('gltf-model', 'url(./3d_models/biblio.glb)');
+    modeloPersonalizado.setAttribute('gltf-model', 'url(./3d_models/Juego monos.glb)');
 
     // 4. Ajustes cruciales: Escala, Posición y Rotación
     // Nota: Los archivos .glb suelen venir con escalas extremas (muy gigantes o microscópicos).
     // Si no ves tu modelo a la primera, juega con estos valores.
     modeloPersonalizado.setAttribute('scale', '0.5 0.5 0.5'); // Mitad de su tamaño original
     modeloPersonalizado.setAttribute('position', '0 0 0'); // Centrado en el marcador
-    modeloPersonalizado.setAttribute('rotation', '0 0 0'); // Rotación en grados (X Y Z)
+    modeloPersonalizado.setAttribute('rotation', '-90 0 0'); // Rotación en grados (X Y Z)
 
     // 5. Añadimos el modelo al marcador
     marker.appendChild(modeloPersonalizado);
